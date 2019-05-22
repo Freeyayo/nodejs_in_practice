c052eb57e20bc163b734b20f213687b86cb98b17
caiconghao <caiconghao@flyfinger.com>
2019-05-23 02:38:56 +0800

    add some codes

---
+++ manipulating_command_line_args/deal_args.js
@@ -0,0 +1,24 @@
+
+const fs = require("fs");
+
+const args = {
+	"-h": displayHelp,
+	"-r": readFile
+}
+
+function displayHelp(){
+	console.log("args : ",args)
+}
+
+function readFile(file){
+	console.log("reading file ...");
+	fs.createReadStream(__dirname + "/" + file).pipe(process.stdout)
+}
+
+if(process.argv.length > 2){
+	process.argv.forEach( (arg, index) => {
+		if(args[arg]){
+			args[arg](process.argv[index + 1])
+		}
+	} )
+}
